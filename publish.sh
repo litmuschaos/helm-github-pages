a8e3a67477a240e4c63a76b72fce11b65d6d3abf
Karthik Satchitanand <karthik.s@mayadata.io>
2021-01-21 20:01:09 +0530

    Update publish.sh (#2)

--- publish.sh
+++ publish.sh
@@ -33,13 +33,15 @@ apk update
 apk add ca-certificates git openssh
 
 echo '>> Installing Helm...'
-cd /tmp/helm/bin
-wget "https://storage.googleapis.com/kubernetes-helm/helm-v${HELM_VERSION}-linux-amd64.tar.gz"
-tar -zxf "helm-v${HELM_VERSION}-linux-amd64.tar.gz"
-chmod +x linux-amd64/helm
-alias helm=/tmp/helm/bin/linux-amd64/helm
+#cd /tmp/helm/bin
+#wget "https://storage.googleapis.com/kubernetes-helm/helm-v${HELM_VERSION}-linux-amd64.tar.gz"
+#tar -zxf "helm-v${HELM_VERSION}-linux-amd64.tar.gz"
+#chmod +x linux-amd64/helm
+#alias helm=/tmp/helm/bin/linux-amd64/helm
+#helm version -c
+#helm init -c
+curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
 helm version -c
-helm init -c
 
 echo ">> Checking out $GITHUB_PAGES_BRANCH branch from $GITHUB_PAGES_REPO"
 cd /tmp/helm/publish
